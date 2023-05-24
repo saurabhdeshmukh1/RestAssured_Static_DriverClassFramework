68337d6ce1d28f239df884c023f54e71cb9323c9
hp <hp@LAPTOP-7KTP9OVS>
2023-05-24 17:52:11 +0530

    restAssuredStatic

---
+++ src/DriverClass.java
@@ -0,0 +1,10 @@
+import java.io.IOException;
+
+import testClassPackage.Post_tc_1;
+
+public class DriverClass {
+	public static void main(String[] args) throws IOException {
+		Post_tc_1.execute();
+	}
+
+}
